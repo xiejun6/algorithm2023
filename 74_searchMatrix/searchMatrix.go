736ce68e1bfdf0358b0cb36ecced34992c84614c
prime <email@example.com>
2023-03-28 11:04:46 +0800

    74. 搜索二维矩阵

---
+++ 74_searchMatrix/searchMatrix.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func searchMatrix(matrix [][]int, target int) bool {
+	m, n := len(matrix), len(matrix[0])
+	left, right := 0, m*n-1
+	for left <= right {
+		mid := (left + right) / 2
+		val := matrix[mid/n][mid%n]
+		if val == target {
+			return true
+		} else if val < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return false
+}
+
+func main() {
+	matrix := [][]int{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}}
+	target := 3
+	fmt.Println(searchMatrix(matrix, target))
+}
