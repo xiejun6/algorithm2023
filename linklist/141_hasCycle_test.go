f7eadbfccb2fc2dbf8024cd204478c7d92efee30
xj <xj@qq.com>
2023-04-12 22:54:09 +0800

    141. 环形链表

---
+++ linklist/141_hasCycle_test.go
@@ -0,0 +1,13 @@
+package linklist
+
+func hasCycle(head *ListNode) bool {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if slow == fast {
+			return true
+		}
+	}
+	return false
+}
