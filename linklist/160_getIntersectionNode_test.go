b216954242136d785c4697111d270896c1493aef
xj <xj@qq.com>
2023-04-13 23:18:08 +0800

    160. 相交链表

---
+++ linklist/160_getIntersectionNode_test.go
@@ -0,0 +1,18 @@
+package linklist
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	p, q := headA, headB
+	for p != q {
+		if p == nil {
+			p = headB
+		} else {
+			p = p.Next
+		}
+		if q == nil {
+			q = headA
+		} else {
+			q = q.Next
+		}
+	}
+	return p
+}
