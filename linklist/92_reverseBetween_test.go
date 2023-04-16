05e101b2a0fad5d8a580af7071280eabf860c6ac
xj <xj@qq.com>
2023-04-16 17:43:58 +0800

    92. 反转链表 II

--- linklist/92_reverseBetween_test.go
+++ linklist/92_reverseBetween_test.go
@@ -1,6 +1,8 @@
 package linklist
 
-import "testing"
+import (
+	"testing"
+)
 
 func reverseBetween(head *ListNode, left int, right int) *ListNode {
 	dummyHead := &ListNode{Next: head}
@@ -36,9 +38,34 @@ func reverse(head *ListNode) *ListNode {
 	return prev
 }
 
+// cur节点一点一直保持不变,prev一直保持不变
+// 把cur.next的节点插入到prev的next
+// 更新cur.next
+func reverseBetween2(head *ListNode, left int, right int) *ListNode {
+	dummyHead := &ListNode{Next: head}
+	prev := dummyHead
+	for i := 1; i < left; i++ {
+		prev = prev.Next
+	}
+	//需要翻转的第一个节点(cur节点一直没变过)
+	cur := prev.Next
+	for i := left; i < right; i++ {
+		nxt := cur.Next
+		cur.Next = nxt.Next
+		nxt.Next = prev.Next
+		prev.Next = nxt
+	}
+	return dummyHead.Next
+}
+
 func TestReverseBetween(t *testing.T) {
 	data := []int{1, 2, 3, 4, 5}
 	head := MakeList(data)
 	newHead := reverseBetween(head, 2, 4)
+	PrintList(newHead)
+
+	data = []int{1, 2, 3, 4, 5}
+	head = MakeList(data)
+	newHead = reverseBetween2(head, 2, 4)
 	PrintList(newHead)
 }
