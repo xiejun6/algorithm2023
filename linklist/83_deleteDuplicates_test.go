e8206c13a44c2fa88061fa6023b7d4f67116fc93
xj <xj@qq.com>
2023-04-17 23:17:59 +0800

    83. 删除排序链表中的重复元素

---
+++ linklist/83_deleteDuplicates_test.go
@@ -0,0 +1,62 @@
+package linklist
+
+import (
+	"fmt"
+	"testing"
+)
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	p := head.Next
+	prev := head
+	for p != nil {
+		if p.Val != prev.Val {
+			prev.Next = p
+			prev = p
+		} else {
+			prev.Next = nil
+		}
+		p = p.Next
+	}
+	return head
+}
+
+func deleteDuplicates2(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	cur := head
+	for cur.Next != nil {
+		if cur.Val == cur.Next.Val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	return head
+}
+
+func TestDeleteDuplicates(t *testing.T) {
+	data := []int{1, 1, 2}
+	head := MakeList(data)
+	newHead := deleteDuplicates(head)
+	PrintList(newHead)
+
+	data = []int{1, 1, 2, 3, 3}
+	head = MakeList(data)
+	newHead = deleteDuplicates(head)
+	PrintList(newHead)
+
+	fmt.Println("=====================")
+	data = []int{1, 1, 2}
+	head = MakeList(data)
+	newHead = deleteDuplicates(head)
+	PrintList(newHead)
+
+	data = []int{1, 1, 2, 3, 3}
+	head = MakeList(data)
+	newHead = deleteDuplicates(head)
+	PrintList(newHead)
+}
