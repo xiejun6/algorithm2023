a823bb706769594120e4880c4caaf167703479a7
xj <xj@qq.com>
2023-04-25 23:26:40 +0800

    105. 从前序与中序遍历序列构造二叉树

---
+++ binaryTree/105_buildTree/buildTree.go
@@ -0,0 +1,24 @@
+package _05_buildTree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+	root := &TreeNode{Val: preorder[0]}
+	index := 0
+	for i, v := range inorder {
+		if v == preorder[0] {
+			index = i
+			break
+		}
+	}
+	root.Left = buildTree(preorder[1:index+1], inorder[:index])
+	root.Right = buildTree(preorder[index+1:], inorder[index+1:])
+	return root
+}
