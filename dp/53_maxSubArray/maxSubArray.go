bb6887e5420e5ac853dce64c89cbe75d5058b4a4
xj <xj@qq.com>
2023-05-08 23:14:13 +0800

    53. 最大子数组和

---
+++ dp/53_maxSubArray/maxSubArray.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func maxSubArray(nums []int) int {
+	ans := nums[0]
+	sum := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if sum > 0 {
+			sum += nums[i]
+		} else {
+			sum = nums[i]
+		}
+		if sum > ans {
+			ans = sum
+		}
+	}
+	return ans
+}
+
+func main() {
+	nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	fmt.Println(maxSubArray(nums))
+}
