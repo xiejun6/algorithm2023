bbe41bfe08fa7d87efa806a54df1ab87bc6d147f
xj <xj@qq.com>
2023-05-21 17:56:27 +0800

    746. 使用最小花费爬楼梯

--- dp/343_integerBreak/integerBreak.go
+++ dp/343_integerBreak/integerBreak.go
@@ -21,6 +21,24 @@ func integerBreak(n int) int {
 	return val
 }
 
+func integerBreak2(n int) int {
+	dp := make([]int, n+1)
+	dp[1] = 1
+	for i := 2; i <= n; i++ {
+		for j := 1; j < i; j++ {
+			dp[i] = max(dp[i], max(dp[i-j]*j, (i-j)*j))
+		}
+	}
+	return dp[n]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
 func main() {
 	fmt.Println(integerBreak(10))
+	fmt.Println(integerBreak2(10))
 }
